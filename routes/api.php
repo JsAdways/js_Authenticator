a2b5d4a8af2d0883e0c9718509b108d3eedd5702
chenchien-ming <alvin@js-adways.com.tw>
2024-02-27 10:05:25 +0800

    feat:調整permission api 增加帶入{id}參數

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,7 @@ use Js\Authenticator\Controllers\SystemController;
 Route::group(['prefix' => 'js/api', 'middleware' => 'api'], function() {
     Route::post('/login', [AuthController::class, 'login']);
     Route::middleware(['js-authenticate-middleware-alias'])->group(function () {
-        Route::post('/permission', [AuthController::class, 'login_info']);
+        Route::post('/permission/{id}', [AuthController::class, 'login_info']);
         Route::post('/logout', [AuthController::class, 'logout']);
     });
     Route::post('/system/permission', [SystemController::class, 'get_permission']);
